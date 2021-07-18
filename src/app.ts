9b39ddd1ba7969fdbef1ec48be216f356b1b4c6f
hussainarthuna <hussainarthuna@gmail.com>
2021-07-18 15:00:05 +0300

    Adding the Cors and helmet to the app configuration page

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, {Application, Request, Response, NextFunction} from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from "helmet";
 
 dotenv.config();
 
@@ -7,6 +9,9 @@ dotenv.config();
 const port = process.env.PORT;
 const app: Application = express();
 
+// App Configuration
+app.use(cors());
+app.use(helmet());
 app.get('/', (req: Request, res: Response) => {
     res.json({
         status: true,
